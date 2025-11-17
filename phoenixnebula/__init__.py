b48219aa21241668fb5f8885a91e5f8e497be416
Salih12334 <salihyilboga98@gmail.com>
2025-11-17 17:33:44 +0300

    fixed

--- phoenixnebula/__init__.py
+++ phoenixnebula/__init__.py
@@ -1,7 +1,7 @@
 """phoenixnebula - A feature-rich, customizable Unix shell."""
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
+__author__ = "Salih"
 __license__ = "MIT"
 
 from .phoenixnebula import main
